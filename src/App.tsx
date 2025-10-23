1092f92d0a842a9b302544803bab9b1ae316b07b
carlos <c.marcos@rpalatam.com.pe>
2025-10-23 09:48:58 -0500

    feat: estilos nuevos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "./scss/_fonts.scss";
+
 import "./scss/header.scss";
 import "./scss/universe-header.scss";
 import "./scss/FooterCredits.scss";
@@ -16,18 +18,22 @@ type View = "general" | "matches";
 const App = () => {
   const [view, setView] = useState<View>("general");
 
+  // base pública: respeta tu "homepage" en package.json
+  const base = process.env.PUBLIC_URL || "";
+
   return (
     <>
       <Header />
+
       <UniverseHeader
-        bgUrl="https://elcomercio.pe/resizer/v2/6YPDFIHE3VDDRHWLZY7QZXBSTA.png?auth=49375be2f3ffd8da51a1fb192ee7b23b3dc6be223c45f95e0fea477f8d14b25e"
+        bgUrl={`${base}/img/universoBicolor.png`}
+        mobileBgUrl={`${base}/img/MOBILE.png`}
         onClickGeneral={() => setView("general")}
         onClickMatches={() => setView("matches")}
-        mobileBgUrl="https://elcomercio.pe/resizer/v2/7QNEOIXVEFF6NJIFLLU35LYS5U.png?auth=528b607386375ca4d2d773d4b8e352498fe31358327b2eefa4eafe32fd9b08f4"
       />
 
-      {view === "general" ? <PlayersPage /> 
-      : <MatchesPage />}
+      {view === "general" ? <PlayersPage /> : <MatchesPage />}
+
       <FooterCredits
         logoUrl="https://d1ts5g4ys243sh.cloudfront.net/proyectos_especiales_prod/especiales/fichajes-futbol-peruano-transferencias-ventas-prestamos-pases-libres-2005-actualidad-historial/img/logo.png"
         logoAlt="El Comercio"
